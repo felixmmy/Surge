cd83737a72902747512a542339c36949424f43ee
felixmmy <76590185+felixmmy@users.noreply.github.com>
2024-01-12 13:05:09 +0800

    Update QiYou.js

--- Module/Panel/QiYou/QiYou.js
+++ Module/Panel/QiYou/QiYou.js
@@ -2,7 +2,7 @@
 * 作者：felix
 * 更新时间：2024年1月8日
 **********/
-var region = 'jiangsu/changzhou/zhonglou'
+var region = 'shanxi-3/xian'
 
 if (typeof $argument !== 'undefined' && $argument !== '') {
     region = $argument
